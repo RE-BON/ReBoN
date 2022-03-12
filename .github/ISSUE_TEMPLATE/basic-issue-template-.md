ad6cde9e00bd56b6242fc51834df558d53af754d
Air Junseo <air.junseo@gmail.com>
2022-03-12 16:54:07 +0900

    [#1] 백엔드 초기 환경 설정 (#33)
    * docs: issue basic template 생성
    
    * docs: basic pr template 생성
    
    * feat[#1]: initial setting

---
+++ .github/ISSUE_TEMPLATE/basic-issue-template-.md
@@ -0,0 +1,15 @@
+---
+name: 'Basic Issue Template '
+about: 기본 Issue 템플릿
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+### Description
+
+### Todo
+- [ ] task
+
+### ETC
---
+++ backend/src/main/java/com/handong/rebon/RebonApplication.java
@@ -0,0 +1,13 @@
+package com.handong.rebon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebonApplication.class, args);
+    }
+
+}
