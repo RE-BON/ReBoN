7e851fc4808d772631c46a33a43996132c7d763f
PyeongGangKim <21500185@handong.edu>
2022-03-15 20:29:12 +0900

    feat[#17]: review title format exception 추가

--- backend/src/main/java/com/handong/rebon/exception/review/ReviewContentFormatException.java
+++ backend/src/main/java/com/handong/rebon/exception/review/ReviewContentFormatException.java
@@ -1,2 +1,10 @@
-package com.handong.rebon.exception.review;public class ReviewContentFormatException {
+package com.handong.rebon.exception.review;
+
+import org.springframework.http.HttpStatus;
+
+public class ReviewContentFormatException extends ReviewException {
+
+    public ReviewContentFormatException() {
+        super("잘못된 리뷰 내용입니다.", HttpStatus.BAD_REQUEST);
+    }
 }
--- backend/src/main/java/com/handong/rebon/exception/review/ReviewException.java
+++ backend/src/main/java/com/handong/rebon/exception/review/ReviewException.java
@@ -1,2 +1,11 @@
-package com.handong.rebon.exception.review;public class ReviewException {
+package com.handong.rebon.exception.review;
+
+import com.handong.rebon.exception.RebonException;
+import org.springframework.http.HttpStatus;
+
+public abstract class ReviewException extends RebonException {
+
+    protected ReviewException(String message, HttpStatus httpStatus) {
+        super(message, httpStatus);
+    }
 }
