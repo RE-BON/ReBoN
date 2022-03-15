1b31996770e78200bba48d6d27e9089c36c2c19d
PyeongGangKim <21500185@handong.edu>
2022-03-15 20:45:02 +0900

    feat[#17]: ReviewContent 생성 구현

--- backend/src/main/java/com/handong/rebon/review/domain/content/ReviewContent.java
+++ backend/src/main/java/com/handong/rebon/review/domain/content/ReviewContent.java
@@ -1,10 +1,50 @@
 package com.handong.rebon.review.domain.content;
 
-import javax.persistence.Embeddable;
+import com.handong.rebon.exception.review.ReviewContentFormatException;
+import com.handong.rebon.exception.review.ReviewTitleFormatException;
+import lombok.Getter;
 
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Getter
 @Embeddable
 public class ReviewContent {
+
     private String title;
     private String content;
     private String tip;
+
+    protected ReviewContent() {
+    }
+
+    public ReviewContent(String title, String content) {
+        IsNotValidtitleAndContent(title, content);
+        this.title = title;
+        this.content = content;
+    }
+
+    public ReviewContent(String title, String content, String tip) {
+        this(title,content);
+        this.tip = tip;
+    }
+
+    private boolean isNotValidTitle(String title) {
+        return Objects.isNull(title) ||
+                title.isBlank();
+    }
+
+    private boolean isNotValidContent(String content) {
+        return Objects.isNull(content) ||
+                content.isBlank();
+    }
+
+    private void IsNotValidtitleAndContent(String title, String content) throws ReviewTitleFormatException, ReviewContentFormatException {
+        if(isNotValidTitle(title)){
+            throw new ReviewTitleFormatException();
+        }
+        if(isNotValidContent(content)){
+            throw new ReviewContentFormatException();
+        }
+    }
 }
