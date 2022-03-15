adc968170df80ad3ec13dc928ec685defaafdbda
Peace <49421197+PyeongGangKim@users.noreply.github.com>
2022-03-16 00:26:23 +0900

    feat[#41]: BaseEntity 생성 (#45)

---
+++ backend/src/main/java/com/handong/rebon/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.handong.rebon.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    @LastModifiedDate
+    LocalDateTime updatedAt;
+}
