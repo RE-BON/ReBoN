2d03c7952b9aad23c881362bedc14b812d7e42c4
PyeongGangKim <21500185@handong.edu>
2022-03-16 20:29:40 +0900

    test[#17]: review title과 content 빈 문자나 null 내용 테스트

--- backend/src/test/java/com/handong/rebon/review/domain/content/ReviewContentTest.java
+++ backend/src/test/java/com/handong/rebon/review/domain/content/ReviewContentTest.java
@@ -1,4 +1,33 @@
+package com.handong.rebon.review.domain.content;
+
+import com.handong.rebon.exception.review.ReviewContentFormatException;
+import com.handong.rebon.exception.review.ReviewTitleFormatException;
+import com.handong.rebon.review.domain.Review;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReviewContentTest {
-  
+    
+    @Test
+    @DisplayName("리뷰의 title은 null이거나 빈 문자열(공백만 있는 문자열 포함)이면 생성할 수 없다.")
+    public void reviewTitleIsNullOrEmptyException() {
+        //given
+        //when, then
+        assertThatThrownBy(() -> Review.builder().reviewContent(new ReviewContent("", "너무 좋아요"))
+                .build())
+                .isInstanceOf(ReviewTitleFormatException.class);
+    }
+
+    @Test
+    @DisplayName("리뷰의 content는 null이거나 빈 문자열(공백만 있는 문자열 포함)이면 생성할 수 없다.")
+    public void reviewContentIsNullOrEmptyException() {
+        //given
+        //when, then
+        assertThatThrownBy(() -> Review.builder().reviewContent(new ReviewContent("맛집입니다.", null))
+                .build())
+                .isInstanceOf(ReviewContentFormatException.class);
+    }
 }
