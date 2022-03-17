c07742caab51d803983d37089377faf8d7954a99
안유진 <Eugenius1st@gmail.com>
2022-03-17 17:07:58 +0900

    style[#49]: search 페이지 레이아웃 작업 - index 페이지 제작 - tag 컴포넌트 추가 제작 필요

--- FrontEnd/rebon-frontend/src/pages/Search/index.js
+++ FrontEnd/rebon-frontend/src/pages/Search/index.js
@@ -1,3 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Tags from './Tags';
+import '../../styles/search.css';
+// 이 콘텐츠 이름은 다른 분들것과 따라 맞추자
 export default function Search() {
-  return <div>ddd</div>;
+  return (
+    <div className="search-wrapper">
+      <div className="search-wrapper-content">
+        <div className="title">
+          가고 싶은 지역을 입력해서 <br />
+          <span>맛집, 숙소</span>를 찾아보세요!
+        </div>
+
+        <div className="input-bar">
+          <input />
+          <FontAwesomeIcon icon={faSearch} className="search" />
+        </div>
+
+        <Tags />
+      </div>
+    </div>
+  );
 }
