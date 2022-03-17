ca7ba139480e3d1bac6c3e3cab15224dcd387b58
안유진 <Eugenius1st@gmail.com>
2022-03-17 15:00:45 +0900

    style[#48]: components/Header/index 생성
    - header 구현을 위해 폴더와 index 파일 생성
    - header.css 파일 생성
    - Router에는 편의상 component를 import 시켰다. 추후 바꿔야 한다

--- FrontEnd/rebon-frontend/src/Router.js
+++ FrontEnd/rebon-frontend/src/Router.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './pages/Detail';
-
+import Header from './components/Header';
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/detail" element={<Detail />} />
+        <Route path="/header" element={<Header />} />
       </Routes>
     </BrowserRouter>
   );
