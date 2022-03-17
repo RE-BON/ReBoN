ae342139ecd48c62853215b0db00073664fe1d8f
Moon-HaHyeon <64579247+hagus99@users.noreply.github.com>
2022-03-17 21:01:05 +0900

    feat[29]: main 레이아웃 생성 (#55)
    main 레이아웃 생성 (#55)

--- FrontEnd/rebon-frontend/src/Router.js
+++ FrontEnd/rebon-frontend/src/Router.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './pages/Detail';
+import Main from './pages/Main';
 import Search from './pages/Search';
 
 function Router() {
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route path="/detail" element={<Detail />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
