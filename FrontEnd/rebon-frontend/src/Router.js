de4b4974e833cfa7eaf941ff3438e1470fbd9620
안유진 <Eugenius1st@gmail.com>
2022-03-17 16:51:33 +0900

    style[#49]: pages/Search 폴더 및 파일 생성 - Tags 파일과 Contents 파일 생성 - Router에 search 경로 추가

--- FrontEnd/rebon-frontend/src/Router.js
+++ FrontEnd/rebon-frontend/src/Router.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './pages/Detail';
-
+import Search from './pages/Search';
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/detail" element={<Detail />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
