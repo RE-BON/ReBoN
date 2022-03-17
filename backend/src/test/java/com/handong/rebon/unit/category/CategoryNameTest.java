2a82f63425d259cb9c0ca4b0244be8e803efb14c
gomster <gomster96@gmail.com>
2022-03-17 20:28:25 +0900

    test: CategoryNameTest 구현
    - Category domain 을 생성할 때 빈 문자열이 이름으로 들어오면 exception이 발생하도록 테스트를 구현하였다.

---
+++ backend/src/test/java/com/handong/rebon/unit/category/CategoryNameTest.java
@@ -0,0 +1,18 @@
+package com.handong.rebon.unit.category;
+
+import com.handong.rebon.exception.category.CategoryNoParentException;
+import com.handong.rebon.shop.domain.category.Category;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CategoryNameTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("카테고리의 이름이 비어있거나 공백으로 들어온 경우 등록할 수 없다.")
+    void blankCategoryName(String name){
+        Assertions.assertThatThrownBy(() -> Category.builder().name(name).build())
+                .isInstanceOf(CategoryNoParentException.class);
+    }
+}
