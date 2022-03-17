3d062446f95d9216d4e660210b63499b83d4529e
안유진 <Eugenius1st@gmail.com>
2022-03-17 15:36:56 +0900

    style[#48]: header레이아웃 - header/index.js 작업 마무리 - header/searchmodal 폴더 생성

---
+++ FrontEnd/rebon-frontend/src/components/Header/SearchModal/index.js
@@ -0,0 +1,3 @@
+export default function SearchModal() {
+  return <div>ddd</div>;
+}
--- FrontEnd/rebon-frontend/src/components/Header/index.js
+++ FrontEnd/rebon-frontend/src/components/Header/index.js
@@ -1,3 +1,34 @@
+import '../../styles/header.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import SearchModal from './SearchModal';
 export default function Header() {
-  return <div>ddd</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  function Login(e) {
+    setIsLogin(true);
+  }
+  return (
+    <div className="header-wrapper">
+      <header>
+        <div className="header-logo">dd</div>
+        {isLogin ? (
+          <div className="header-log-on">
+            <SearchModal />
+            <div className="header-log-on-profile">
+              <div>
+                <FontAwesomeIcon icon={faUser} className="userProfile" />
+              </div>
+              홍길동
+            </div>
+          </div>
+        ) : (
+          <div onClick={Login} className="header-log-off">
+            <span>로그인</span>
+            <span>회원가입</span>
+          </div>
+        )}
+      </header>
+    </div>
+  );
 }
