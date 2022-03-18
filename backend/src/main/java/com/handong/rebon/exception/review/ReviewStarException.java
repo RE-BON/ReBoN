594f8f035927d4d5aec5b28091b8474d090e29bf
PyeongGangKim <21500185@handong.edu>
2022-03-18 22:50:07 +0900

    refactor[#17]: formatting 적용

--- backend/src/main/java/com/handong/rebon/exception/review/ReviewStarException.java
+++ backend/src/main/java/com/handong/rebon/exception/review/ReviewStarException.java
@@ -2,7 +2,7 @@ package com.handong.rebon.exception.review;
 
 import org.springframework.http.HttpStatus;
 
-public class ReviewStarException extends ReviewException{
+public class ReviewStarException extends ReviewException {
     public ReviewStarException() {
         super("별점은 음수가 될 수 없습니다.", HttpStatus.BAD_REQUEST);
     }
