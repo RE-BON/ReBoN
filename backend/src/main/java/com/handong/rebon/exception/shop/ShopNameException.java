afbfd4ace2b1a13d2f3794db6a57645894a8fc20
kimjunseo <air.junseo@gmail.com>
2022-03-18 14:57:16 +0900

    refactor[#5]: 예외 메세지 변경

--- backend/src/main/java/com/handong/rebon/exception/shop/ShopNameException.java
+++ backend/src/main/java/com/handong/rebon/exception/shop/ShopNameException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 public class ShopNameException extends ShopException {
 
     public ShopNameException() {
-        super("잘못된 이름입니다.", HttpStatus.BAD_REQUEST);
+        super("빈 문자열이 올 수 없습니다.", HttpStatus.BAD_REQUEST);
     }
 }
