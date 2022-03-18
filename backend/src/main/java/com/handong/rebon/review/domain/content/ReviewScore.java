fbc7afae85fdcbfa0ff9b69bf507f5d2233acdef
PyeongGangKim <21500185@handong.edu>
2022-03-18 21:52:23 +0900

    feat[#17]: reviewScore exception 추가
    별점, 공감 수가 음수로 들어왔을 시, exception 반환

--- backend/src/main/java/com/handong/rebon/review/domain/content/ReviewScore.java
+++ backend/src/main/java/com/handong/rebon/review/domain/content/ReviewScore.java
@@ -6,11 +6,28 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
+import com.handong.rebon.exception.review.ReviewLikeCountException;
+import com.handong.rebon.exception.review.ReviewStarException;
+
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
 public class ReviewScore {
     private double star;
-    private int likeCount;
+    private int likeCount; // empathyCount로 바꾸는 건 어떨까?
+
+    public ReviewScore(double star, int likeCount) {
+        validatesNegative(star,likeCount);
+        this.star = star;
+        this.likeCount = likeCount;
+    }
+
+    private void validatesNegative(double star, int likeCount) {
+        if(star < 0) {
+            throw new ReviewStarException();
+        }
+        if(likeCount < 0) {
+            throw new ReviewLikeCountException();
+        }
+    }
 }
