9352f14572ccf843181b315dbd163dd155cb09b9
PyeongGangKim <21500185@handong.edu>
2022-03-18 21:54:38 +0900

    test[#17]: reviewContent 단위테스트
    review content 단위테스트 시, parameter로 null과 empty source 전달

--- backend/src/test/java/com/handong/rebon/review/domain/content/ReviewContentTest.java
+++ backend/src/test/java/com/handong/rebon/review/domain/content/ReviewContentTest.java
@@ -2,32 +2,33 @@ package com.handong.rebon.review.domain.content;
 
 import com.handong.rebon.exception.review.ReviewContentFormatException;
 import com.handong.rebon.exception.review.ReviewTitleFormatException;
-import com.handong.rebon.review.domain.Review;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class ReviewContentTest {
-    
-    @Test
+
+    @ParameterizedTest
+    @NullAndEmptySource
     @DisplayName("리뷰의 title은 null이거나 빈 문자열(공백만 있는 문자열 포함)이면 생성할 수 없다.")
-    public void reviewTitleIsNullOrEmptyException() {
+    public void reviewTitleIsNullOrEmptyException(String title) {
         //given
         //when, then
-        assertThatThrownBy(() -> Review.builder().reviewContent(new ReviewContent("", "너무 좋아요"))
-                .build())
+
+        assertThatThrownBy(() ->new ReviewContent(title, "너무 좋아요"))
                 .isInstanceOf(ReviewTitleFormatException.class);
     }
 
-    @Test
+    @ParameterizedTest
+    @NullAndEmptySource
     @DisplayName("리뷰의 content는 null이거나 빈 문자열(공백만 있는 문자열 포함)이면 생성할 수 없다.")
-    public void reviewContentIsNullOrEmptyException() {
+    public void reviewContentIsNullOrEmptyException(String content) {
         //given
         //when, then
-        assertThatThrownBy(() -> Review.builder().reviewContent(new ReviewContent("맛집입니다.", null))
-                .build())
+        assertThatThrownBy(() -> new ReviewContent("맛집입니다.", content))
                 .isInstanceOf(ReviewContentFormatException.class);
     }
 }
