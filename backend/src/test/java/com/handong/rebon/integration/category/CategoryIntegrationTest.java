bc04f7a06dc6e9ef530b4cff41dc4b8c4fff484e
gomster <gomster96@gmail.com>
2022-03-22 01:32:30 +0900

    test[#4] : 같은 부모 카테고리 추가시, 중복일 경우 예외발생 테스트
    - 같은 부모인 동일 이름의 데이터를 추가시, exception이 발생하는지의 여부를 테스트하였다.

--- backend/src/test/java/com/handong/rebon/integration/category/CategoryIntegrationTest.java
+++ backend/src/test/java/com/handong/rebon/integration/category/CategoryIntegrationTest.java
@@ -89,4 +89,25 @@ public class CategoryIntegrationTest {
         //then
         assertThat(exception.getMessage()).isEqualTo("이미 존재하는 이름의 Category 입니다.");
     }
+
+    @Test
+    @DisplayName("같은 부모 카테고리를 생성할 때 이미 존재하는 카테고리라면 예외 발생")
+    public void createSameParentDuplicateCategory() {
+
+        //given
+        String parentName = "테스트식당";
+        String createdName = "테스트한식";
+        Long parentId = categoryService.create(parentName);
+        CategoryRequestDto categoryRequestDto = CategoryRequestDto.builder()
+                                                                  .parentId(parentId)
+                                                                  .name(createdName)
+                                                                  .build();
+        categoryService.create(categoryRequestDto);
+
+        //when
+        CategoryExistException exception = assertThrows(CategoryExistException.class, () -> categoryService.create(categoryRequestDto));
+
+        //then
+        assertThat(exception.getMessage()).isEqualTo("이미 존재하는 이름의 Category 입니다.");
+    }
 }
