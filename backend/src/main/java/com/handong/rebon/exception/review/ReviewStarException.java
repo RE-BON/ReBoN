1395c3610e0c6099876ae951c96f0b487fe6e2aa
PyeongGangKim <21500185@handong.edu>
2022-03-21 18:09:44 +0900

    refactor[#17]: exception message 변경

--- backend/src/main/java/com/handong/rebon/exception/review/ReviewStarException.java
+++ backend/src/main/java/com/handong/rebon/exception/review/ReviewStarException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class ReviewStarException extends ReviewException {
     public ReviewStarException() {
-        super("별점은 음수가 될 수 없습니다.", HttpStatus.BAD_REQUEST);
+        super("별점은 범위를 벗어났습니다.", HttpStatus.BAD_REQUEST);
     }
 }
