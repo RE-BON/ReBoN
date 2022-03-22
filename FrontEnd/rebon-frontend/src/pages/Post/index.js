855e90a3e47ac56ef364c3990c1ccba6aa659a5c
안유진 <Eugenius1st@gmail.com>
2022-03-22 23:18:15 +0900

    style[#50]: Post 페이지 레이아웃 생성
    - 전체 레이아웃 작업 완료
    - 별점 구현 미완성

--- FrontEnd/rebon-frontend/src/pages/Post/index.js
+++ FrontEnd/rebon-frontend/src/pages/Post/index.js
@@ -1,16 +1,46 @@
-import Header from '../../components/Header';
 import '../../styles/post.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import Header from '../../components/Header';
+
 export default function Post() {
   return (
     <>
       <Header />
       <div className="post-wrapper">
-        <div>리뷰쓰기</div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="post-title">리뷰쓰기</div>
+        <div className="post-star">
+          <FontAwesomeIcon icon={faStar} size="2x" />
+          <FontAwesomeIcon icon={faStar} size="2x" />
+          <FontAwesomeIcon icon={faStar} size="2x" />
+          <FontAwesomeIcon icon={faStar} size="2x" />
+          <FontAwesomeIcon icon={faStar} size="2x" />
+        </div>
+
+        <div className="post-tip">
+          <div className="post-tip-box">나만의 꿀팁</div>
+          <input placeholder=" ''마라도 횟집''에 대한 나만의 꿀팁을 적어주세요(ex.2층 창가자리 뷰가 예뻐요)" maxlength="500" />
+        </div>
+        <div className="post-tip-count">1/500</div>
+
+        <div className="post-review">
+          <textarea rows="6" placeholder="'마라도 횟집'에 대한 리뷰를 적어주세요" maxlength="500"></textarea>
+          <div className="post-review-count">1/1000</div>
+        </div>
+
+        <div className="post-attach">
+          <div className="post-attach-contents">
+            <div className="post-attach-image">
+              <FontAwesomeIcon icon={faPlus} size="2x" />
+            </div>
+            <div className="post-attach-count">1/10</div>
+          </div>
+        </div>
+        <div className="post-button">
+          <div className="post-button-cancel">취소</div>
+          <div className="post-button-finish">작성완료</div>
+        </div>
       </div>
     </>
   );
