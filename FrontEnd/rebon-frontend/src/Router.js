51c6086773af11efe04d33259d212701632e69ae
안유진 <Eugenius1st@gmail.com>
2022-03-22 18:00:11 +0900

    style[#50]: Header 컴포넌트 단위 %로 수정
    - Header/index.js className  변경 (header-log-lon -> header-logon)
    - header-search-bar.css 단위 %로 수정
    - header.css 단위 %로 수정

--- FrontEnd/rebon-frontend/src/Router.js
+++ FrontEnd/rebon-frontend/src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './pages/Detail';
 import Main from './pages/Main';
 import Search from './pages/Search';
+import Post from './components/Header';
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
         <Route path="/detail" element={<Detail />} />
         <Route path="/search" element={<Search />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/post" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
