24e785245c8fe2f25b3c3f1dd46bcd952618a07a
PyeongGangKim <21500185@handong.edu>
2022-03-23 15:00:02 +0900

    refactor[#17]: 사용하지 않는 패키지, class 제거

--- backend/src/main/java/com/handong/rebon/review/presentation/dto/ReviewAssembler.java
+++ backend/src/main/java/com/handong/rebon/review/presentation/dto/ReviewAssembler.java
@@ -1,25 +1,13 @@
 package com.handong.rebon.review.presentation.dto;
 
 import com.handong.rebon.review.application.dto.request.ReviewCreateRequestDto;
-import com.handong.rebon.review.application.dto.response.ReviewResponseDto;
 import com.handong.rebon.review.presentation.dto.request.ReviewRequest;
-import com.handong.rebon.review.presentation.dto.response.ReviewResponse;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReviewAssembler {
-
-    public static ReviewResponse reviewResponse(ReviewResponseDto reviewResponseDto) {
-        return ReviewResponse.builder()
-                             .id(reviewResponseDto.getId())
-                             .shopName(reviewResponseDto.getShopName())
-                             .authorName(reviewResponseDto.getShopName())
-                             .title(reviewResponseDto.getTitle())
-                             .content(reviewResponseDto.getContent())
-                             .tip(reviewResponseDto.getTip())
-                             .star(reviewResponseDto.getStar())
-                             .empathyCount(reviewResponseDto.getEmpathyCount())
-                             //.images() TODO 추후 구현
-                             .build();
-    }
 
     public static ReviewCreateRequestDto reviewCreateRequestDto(Long memberId, Long shopId, ReviewRequest reviewRequest) {
         return ReviewCreateRequestDto.builder()
