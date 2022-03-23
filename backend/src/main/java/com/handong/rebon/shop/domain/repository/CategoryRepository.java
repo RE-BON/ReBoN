1f551733b6d53d246872bbe5132e296734e26415
gomster <gomster96@gmail.com>
2022-03-23 11:37:59 +0900

    refactor[#4] : 사용하지 않는 메서드 제거

--- backend/src/main/java/com/handong/rebon/shop/domain/repository/CategoryRepository.java
+++ backend/src/main/java/com/handong/rebon/shop/domain/repository/CategoryRepository.java
@@ -4,11 +4,7 @@ import com.handong.rebon.shop.domain.category.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    Optional<Category> findByName(String name);
 
     boolean existsByName(String name);
 }
