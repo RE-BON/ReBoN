d02df3aee58e97b445b06e193958ceb30c6a2aa6
leehyelim0691 <48082589+leehyelim0691@users.noreply.github.com>
2022-03-23 21:12:50 +0900

    feat[#61]: Footprint 레이아웃 생성 (#69)
    * feat[#3]: 기본 구조 생성
    
    * feat[#43]: review 레이아웃 생
    
    * feat[#32]: detail 레이아웃 생
    
    * feat[#32]: detail 레이아웃 생성
    
    * style[#32]: detail 레이아웃 생성
    
    * fix[#32]: review style 수정
    
    * fix[#32]: detail 레이아웃 수정
    
    * feat[#61]: Footprint 레이아웃 생성
    
    * fix[#61]: Footprint 레이아웃 수정

--- FrontEnd/rebon-frontend/src/Router.js
+++ FrontEnd/rebon-frontend/src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './pages/Detail';
 import Main from './pages/Main';
 import Search from './pages/Search';
+import Footprint from './pages/Footprint';
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
         <Route path="/detail" element={<Detail />} />
         <Route path="/search" element={<Search />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/footprint" element={<Footprint />} />
       </Routes>
     </BrowserRouter>
   );
