f59888c22ebdb9960142c413daf5b3116cf90a7a
hagus99 <hagus99@kakao.com>
2022-03-23 21:09:38 +0900

    feat[#62]: MypageSide 레이아웃 생성

--- FrontEnd/rebon-frontend/src/Router.js
+++ FrontEnd/rebon-frontend/src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './pages/Detail';
 import Main from './pages/Main';
 import Search from './pages/Search';
+import Mypage from './pages/Mypage';
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
         <Route path="/detail" element={<Detail />} />
         <Route path="/search" element={<Search />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ FrontEnd/rebon-frontend/src/pages/Mypage/index.js
@@ -0,0 +1,11 @@
+import '../../styles/mypage.css';
+
+import SideMenu from './SideMenu';
+
+export default function Mypage() {
+  return (
+    <div className="mypage-wrapper">
+      <SideMenu />
+    </div>
+  );
+}
