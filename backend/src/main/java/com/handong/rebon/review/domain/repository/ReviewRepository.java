36aee42e4a2c58592d2847e078f9db1202842278
PyeongGangKim <21500185@handong.edu>
2022-03-29 20:01:11 +0900

    feat[#56]: member, shop fetch join 추가

--- backend/src/main/java/com/handong/rebon/review/domain/repository/ReviewRepository.java
+++ backend/src/main/java/com/handong/rebon/review/domain/repository/ReviewRepository.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import com.handong.rebon.review.domain.Review;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("select r from Review r where r.reviewContent.content like :keyword or r.reviewContent.tip like :keyword")
-    List<Review> findAllByReviewContentContaining(@Param("keyword") String keyword);
+    @Query("select r from Review r join fetch r.member join fetch r.shop where r.reviewContent.content like :keyword or r.reviewContent.tip like :keyword")
+    List<Review> findAllByReviewContentContaining(@Param("keyword") String keyword, Pageable pageable);
 
-    List<Review> findAllByMemberId(Long memberId);
+    @Query("select r from Review r join fetch r.member join fetch r.shop where r.member.id = :memberId")
+    List<Review> findAllByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
-    List<Review> findAllByShopId(Long shopId);
+    @Query("select r from Review r join fetch r.member join fetch r.shop where r.shop.id = :shopId")
+    List<Review> findAllByShopId(@Param("shopId") Long shopId, Pageable pageable);
 }
