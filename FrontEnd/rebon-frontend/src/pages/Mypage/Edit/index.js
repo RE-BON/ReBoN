3c7fc1baa9fa952898eabd34d570733e7e1b872c
안유진 <Eugenius1st@gmail.com>
2022-03-29 17:05:38 +0900

    style[#83]: Edit 페이지 수정 - Edit 페이지 레이아웃 수정

--- FrontEnd/rebon-frontend/src/pages/Mypage/Edit/index.js
+++ FrontEnd/rebon-frontend/src/pages/Mypage/Edit/index.js
@@ -2,50 +2,54 @@ import '../../../styles/mypage.css';
 import '../../../styles/myedit.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser, faXmark, faCamera } from '@fortawesome/free-solid-svg-icons';
-
+import { faCircleUser, faXmark, faCamera, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 export default function Edit() {
+  const [bgColor, setbgColor] = useState('#c4c4c4');
+  const ChnageBgColor = (e) => {
+    setbgColor(bgColor === '#c4c4c4' ? '#ff6b6c' : '#c4c4c4');
+  };
+
   return (
-    <div className="my-wrapper">
-      <div className="myedit-content-wrapper">
-        <div className="myedit-content-title">
-          <div>회원정보 수정</div>
+    <div>
+      <div className="edit-title">
+        <div>회원정보 수정</div>
+      </div>
+
+      <div className="edit-info">
+        <div className="edit-info-icon">
+          <div className="icon-profile">
+            <FontAwesomeIcon icon={faCircleUser} size="5x" color="#c4c4c4" />
+            <span className="icon-camera">
+              <FontAwesomeIcon icon={faCamera} size="1x" color="white" />
+            </span>
+          </div>
         </div>
 
-        <div className="myedit-content-info">
-          <div className="myedit-content-info-icon">
-            <div className="icon-profile-icon">
-              <FontAwesomeIcon icon={faCircleUser} size="5x" color="#c4c4c4" />
-              <span className="icon-profile-icon-camera">
-                <FontAwesomeIcon icon={faCamera} size="1x" color="white" />
-              </span>
-            </div>
+        <div className="edit-info-title">닉네임</div>
+        <div className="edit-info-name">
+          <input type="text" placeholder="한글로 공백 없이 입력해주세요."></input>
+          <div className="name-button">
+            <FontAwesomeIcon icon={faXmark} size="1x" color="white" />
           </div>
+        </div>
 
-          <div className="myedit-content-info-title">이름</div>
-          <div className="myedit-content-info-name">
-            <input type="text" placeholder="한글로 공백 없이 입력해주세요."></input>
-            <div className="name-button">
-              <FontAwesomeIcon icon={faXmark} size="1x" color="white" />
-            </div>
-          </div>
+        <div className="edit-info-title ">이메일</div>
+        <div className="edit-info-email">ReBon@gmail.com</div>
 
-          <div className="myedit-content-info-title ">이메일</div>
-          <div className="myedit-content-info-email">
-            <input type="text" placeholder="ReBon@gmail.com"></input>
-            <div className="email-button">중복확인</div>
-          </div>
+        <div className="edit-info-notice ">이메일 수신에 동의하시면 Rebon 주식회사가 제공하는 서비스의 업데이트 및 이벤트 소식을 받을 수 있습니다.</div>
 
-          <div className="myedit-content-info-title ">휴대폰 번호</div>
-          <div className="myedit-content-info-phone">
-            <input type="text" placeholder="010-1234-5678"></input>
-            <div className="phone-button">인증번호 요청</div>
-          </div>
+        <div className="edit-info-agree " onClick={ChnageBgColor}>
+          <span className="agree-button" style={{ backgroundColor: bgColor }}>
+            {console.log(bgColor)}
+            <FontAwesomeIcon icon={faCheck} size="1x" />
+          </span>
+          <span>수신 동의하기</span>
+        </div>
 
-          <div className="myedit-content-info-button">
-            <div className="cancel">취소</div>
-            <div className="finished">수정완료</div>
-          </div>
+        <div className="edit-info-submit">
+          <div className="cancel">취소</div>
+          <div className="finished">수정완료</div>
         </div>
       </div>
     </div>
