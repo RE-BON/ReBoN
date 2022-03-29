7bb7db32b3e2fdc2a2263e0a4a15efb68b5c8222
PyeongGangKim <21500185@handong.edu>
2022-03-29 20:01:39 +0900

    feat[#56]: get reviews, review 추가

--- backend/src/main/java/com/handong/rebon/review/presentation/AdminReviewController.java
+++ backend/src/main/java/com/handong/rebon/review/presentation/AdminReviewController.java
@@ -3,6 +3,7 @@ package com.handong.rebon.review.presentation;
 import com.handong.rebon.review.application.ReviewService;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +15,13 @@ public class AdminReviewController {
 
     private final ReviewService reviewService;
 
+    @GetMapping("/")
+    public String getReviews(){
+        return "";
+    }
+
+    @GetMapping("/{reviewId}")
+    public String getReview(){
+        return "";
+    }
 }
