b3a71138bfb7344bce2eb19f451996e0cc386555
PyeongGangKim <21500185@handong.edu>
2022-03-31 15:17:37 +0900

    refactor[#76]: 마케팅 수신 동의 필드 이름 변경

--- backend/src/main/java/com/handong/rebon/member/domain/Member.java
+++ backend/src/main/java/com/handong/rebon/member/domain/Member.java
@@ -29,7 +29,7 @@ public class Member {
     @OneToMany(mappedBy = "member")
     private List<Empathy> empathies;
 
-    private boolean isAgreementMarketingReceptionTerm;
+    private boolean isAgreed;
     private boolean isDeleted;
 
     public Member(Profile profile) {
