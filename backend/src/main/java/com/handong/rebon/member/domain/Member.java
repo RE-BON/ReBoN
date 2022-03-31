3f0a076e008f918f32de1c42330d6b9f1561e6bc
PyeongGangKim <21500185@handong.edu>
2022-04-01 03:18:15 +0900

    feat[#37]: member 어드민 여부, 동일 member 여부 확인 기능 추가

--- backend/src/main/java/com/handong/rebon/member/domain/Member.java
+++ backend/src/main/java/com/handong/rebon/member/domain/Member.java
@@ -1,5 +1,6 @@
 package com.handong.rebon.member.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
 
@@ -7,10 +8,11 @@ import com.handong.rebon.common.BaseEntity;
 import com.handong.rebon.review.domain.empathy.Empathy;
 import com.handong.rebon.shop.domain.like.Likes;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class Member extends BaseEntity {
@@ -24,11 +26,13 @@ public class Member extends BaseEntity {
     @Embedded
     private Profile profile;
 
+    @Builder.Default
     @OneToMany(mappedBy = "member")
-    private List<Likes> likes;
+    private List<Likes> likes = new ArrayList<>();
 
+    @Builder.Default
     @OneToMany(mappedBy = "member")
-    private List<Empathy> empathies;
+    private List<Empathy> empathies = new ArrayList<>();
 
     private boolean isAgreed;
 
@@ -36,6 +40,14 @@ public class Member extends BaseEntity {
 
     public Member(Profile profile) {
         this.profile = profile;
+    }
+
+    public boolean isSame(Member member) {
+        return this.id == member.getId();
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
     }
 
     public String getNickName() {
