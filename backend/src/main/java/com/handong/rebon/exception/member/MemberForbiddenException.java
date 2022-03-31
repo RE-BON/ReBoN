809e21754ddf432583a37cab420fd25f8f53fe9b
PyeongGangKim <21500185@handong.edu>
2022-04-01 03:14:28 +0900

    refactor[#37]: reformatting

--- backend/src/main/java/com/handong/rebon/exception/member/MemberForbiddenException.java
+++ backend/src/main/java/com/handong/rebon/exception/member/MemberForbiddenException.java
@@ -1,2 +1,9 @@
-package com.handong.rebon.exception.member;public class MemberForbiddenException {
+package com.handong.rebon.exception.member;
+
+import org.springframework.http.HttpStatus;
+
+public class MemberForbiddenException extends MemberException {
+    public MemberForbiddenException() {
+        super("권한이 없는 멤버입니다.", HttpStatus.FORBIDDEN);
+    }
 }
--- backend/src/main/java/com/handong/rebon/exception/member/MemberNotFoundException.java
+++ backend/src/main/java/com/handong/rebon/exception/member/MemberNotFoundException.java
@@ -1,2 +1,10 @@
-package com.handong.rebon.exception.member;public class MemberNotFoundException {
+package com.handong.rebon.exception.member;
+
+import org.springframework.http.HttpStatus;
+
+public class MemberNotFoundException extends MemberException {
+
+    public MemberNotFoundException() {
+        super("해당 멤버를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST);
+    }
 }
