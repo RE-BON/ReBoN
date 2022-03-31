d2202d2febc7f17bc419e3f9a60b3d3a02fa864e
PyeongGangKim <21500185@handong.edu>
2022-04-01 01:56:57 +0900

    feat[#56]: static 추가

--- backend/src/main/java/com/handong/rebon/util/StringUtil.java
+++ backend/src/main/java/com/handong/rebon/util/StringUtil.java
@@ -1,5 +1,9 @@
 package com.handong.rebon.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StringUtil {
     public static String makeContainingKeyword(String keyword) {
         StringBuilder stringbuilder = new StringBuilder();
