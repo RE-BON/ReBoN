a0ebb71ecb1a219d8d8f8820a59c5f0806cf2d42
안유진 <Eugenius1st@gmail.com>
2022-04-07 18:02:43 +0900

    [#103]: myPage 모달창 수정

--- FrontEnd/rebon-frontend/src/pages/Mypage/LogoutModal/index.js
+++ FrontEnd/rebon-frontend/src/pages/Mypage/LogoutModal/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Modal, { ModalProvider, BaseModalBackground } from 'styled-react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import LogoutIcon from '@mui/icons-material/Logout';
 import '../../../styles/logout-modal.css';
 export default function LogoutModal() {
   const [isOpen, setIsOpen] = useState(false);
@@ -48,8 +49,8 @@ export default function LogoutModal() {
           <button className="close" onClick={toggleModal}>
             <FontAwesomeIcon icon={faXmark} />
           </button>
-          <div className="logout-image">
-            <image alt="logout-image" src="image/detail.png" />
+          <div className="logout-icon">
+            <LogoutIcon fontSize="large" />
           </div>
           <div className="logout-notice">로그아웃 하시겠습니까?</div>
           <button className="logout-button">확인</button>
@@ -60,7 +61,7 @@ export default function LogoutModal() {
 }
 
 const StyledModal = Modal.styled`
-  width: 25rem;
+  width: 23rem;
   height: 20rem;
   padding : 20px;
   border-radius:20px;
