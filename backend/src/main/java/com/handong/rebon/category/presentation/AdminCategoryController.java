eaee34ca18bf9a919db58ab4fea5763c79014e4a
gomster <gomster96@gmail.com>
2022-04-12 19:51:51 +0900

    feat[#114] : 카테고리 생성, 삭제 Admin Controller 기능 구현

--- backend/src/main/java/com/handong/rebon/category/presentation/AdminCategoryController.java
+++ backend/src/main/java/com/handong/rebon/category/presentation/AdminCategoryController.java
@@ -3,32 +3,64 @@ package com.handong.rebon.category.presentation;
 import java.util.List;
 
 import com.handong.rebon.category.application.CategoryService;
+import com.handong.rebon.category.application.dto.request.CategoryCreateRequestDto;
 import com.handong.rebon.category.application.dto.request.CategoryRequestDto;
 import com.handong.rebon.category.application.dto.response.RootCategoryResponseDto;
 import com.handong.rebon.category.presentation.dto.CategoryAssembler;
+import com.handong.rebon.category.presentation.dto.request.CategoryCreateRequest;
 import com.handong.rebon.category.presentation.dto.request.CategoryRequest;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RequestMapping("/admin")
+@Controller
 public class AdminCategoryController {
 
     private final CategoryService categoryService;
 
-    @DeleteMapping("/category")
-    public String deleteCategory(@RequestBody CategoryRequest request) {
+    @GetMapping("/categories/new")
+    public String categoryCreateForm(Model model){
+
+        List<RootCategoryResponseDto> categories = categoryService.findRootCategoriesAndChildren();
+        model.addAttribute("categories", CategoryAssembler.rootCategoryResponses(categories));
+
+        return "category/createForm";
+    }
+
+    @GetMapping("/categories/remove")
+    public String categoryDeleteForm(Model model){
+
+        List<RootCategoryResponseDto> categories = categoryService.findRootCategoriesAndChildren();
+        model.addAttribute("categories", CategoryAssembler.rootCategoryResponses(categories));
+
+        return "category/deleteForm";
+    }
+
+    @PostMapping("/categories")
+    public String createCategory(CategoryCreateRequest categoryRequest){
+
+        CategoryCreateRequestDto categoryCreateRequestDto = CategoryAssembler.categoryCreateRequestDto(categoryRequest);
+        if(categoryCreateRequestDto.getParentId() == null){
+            categoryService.create(categoryCreateRequestDto.getName());
+        } else {
+            categoryService.create(categoryCreateRequestDto);
+        }
+        return "home";
+    }
+
+    @PostMapping("/remove-categories")
+    public String deleteCategory(CategoryRequest request) {
 
         CategoryRequestDto categoryRequestDto = CategoryAssembler.categoryRequestDto(request);
         categoryService.delete(categoryRequestDto);
 
-        List<RootCategoryResponseDto> rootCategories = categoryService.findRootCategoriesAndChildren();
-
-        // ToDo Admin 페이지 구현 예정
-        return "";
+        return "home";
     }
 }
--- backend/src/main/java/com/handong/rebon/category/presentation/dto/CategoryAssembler.java
+++ backend/src/main/java/com/handong/rebon/category/presentation/dto/CategoryAssembler.java
@@ -3,9 +3,11 @@ package com.handong.rebon.category.presentation.dto;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.handong.rebon.category.application.dto.request.CategoryCreateRequestDto;
 import com.handong.rebon.category.application.dto.request.CategoryRequestDto;
 import com.handong.rebon.category.application.dto.response.ChildCategoryDto;
 import com.handong.rebon.category.application.dto.response.RootCategoryResponseDto;
+import com.handong.rebon.category.presentation.dto.request.CategoryCreateRequest;
 import com.handong.rebon.category.presentation.dto.request.CategoryRequest;
 import com.handong.rebon.category.presentation.dto.response.ChildCategoryResponse;
 import com.handong.rebon.category.presentation.dto.response.RootCategoryResponse;
@@ -32,5 +34,9 @@ public class CategoryAssembler {
 
     public static CategoryRequestDto categoryRequestDto(CategoryRequest request){
         return new CategoryRequestDto(request.getCategoryId());
+    }
+
+    public static CategoryCreateRequestDto categoryCreateRequestDto(CategoryCreateRequest request){
+        return new CategoryCreateRequestDto(request.getParentId(), request.getName());
     }
 }
