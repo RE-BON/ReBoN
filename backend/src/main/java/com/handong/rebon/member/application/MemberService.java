22bef004a7e753e36021d0680f964764ec469785
PyeongGangKim <21500185@handong.edu>
2022-04-13 22:28:55 +0900

    feat[#92]: email과 oauthProvider로 member 찾는 기능 추가

--- backend/src/main/java/com/handong/rebon/member/application/MemberService.java
+++ backend/src/main/java/com/handong/rebon/member/application/MemberService.java
@@ -1,10 +1,13 @@
 package com.handong.rebon.member.application;
 
+import com.handong.rebon.auth.domain.OauthProvider;
 import com.handong.rebon.exception.member.MemberNotFoundException;
+import com.handong.rebon.member.application.dto.MemberCreateDto;
 import com.handong.rebon.member.domain.Member;
 import com.handong.rebon.member.domain.repository.MemberRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,7 +17,21 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     //Review 메서드가 잘 돌아가는지 확인을 위해 임시로 만든 메서드
+    @Transactional(readOnly = true)
     public Member findById(Long id) {
         return memberRepository.findById(id).orElseThrow(MemberNotFoundException::new);
+    }
+
+    @Transactional
+    public Long save(MemberCreateDto memberCreateDto) {
+        OauthProvider oauthProvider = OauthProvider.valueOf(memberCreateDto.getOauthProvider());
+        String email = memberCreateDto.getEmail();
+        String nickname = memberCreateDto.getNickname();
+        boolean isAgreed = memberCreateDto.isAgreed();
+
+        Member member = new Member(email,nickname,isAgreed,oauthProvider);
+
+        Member savedMember = memberRepository.save(member);
+        return savedMember.getId(); // jwt,id 같이.
     }
 }
--- backend/src/main/java/com/handong/rebon/member/domain/repository/MemberRepository.java
+++ backend/src/main/java/com/handong/rebon/member/domain/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.handong.rebon.member.domain.repository;
 
 import java.util.Optional;
 
+import com.handong.rebon.auth.domain.OauthProvider;
 import com.handong.rebon.member.domain.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    @Query("select m from Member m where m.profile.email =:email")
-    Optional<Member> findByEmail(@Param("email") String email);
+    @Query("select m from Member m where m.profile.email =:email and m.oauthProvider:= oauthProvide")
+    Optional<Member> findByEmail(@Param("email") String email, @Param("oauthProvider")OauthProvider oauthProvider);
 }
