cb4691535cdc4287f6fe93794afb6b409e5e83b6
PyeongGangKim <21500185@handong.edu>
2022-04-13 22:26:09 +0900

    feat[#92]: email과 oauthProvider로 member 찾는 기능

--- backend/src/main/java/com/handong/rebon/member/domain/repository/MemberRepository.java
+++ backend/src/main/java/com/handong/rebon/member/domain/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package com.handong.rebon.member.domain.repository;
 
+import java.util.Optional;
+
 import com.handong.rebon.member.domain.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("select m from Member m where m.profile.email =:email")
+    Optional<Member> findByEmail(@Param("email") String email);
 }
