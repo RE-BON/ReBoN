90e3498f361ec8b34b0854dade60d8a6a4a40e15
PyeongGangKim <21500185@handong.edu>
2022-04-27 16:09:10 +0900

    test[#92]: 외부 server를 mocking해서 test
    결국 localhost에서 주고 받는 테스트이지만, 다른 포트에 서버를 띄어놓아, 외부 서버와 api 통신하는 것처럼 사용

--- backend/src/test/java/com/handong/rebon/acceptance/auth/AuthAcceptanceTest.java
+++ backend/src/test/java/com/handong/rebon/acceptance/auth/AuthAcceptanceTest.java
@@ -3,77 +3,103 @@ package com.handong.rebon.acceptance.auth;
 import java.util.Map;
 
 import com.handong.rebon.acceptance.AcceptanceTest;
-import com.handong.rebon.auth.domain.GoogleUserInfo;
 import com.handong.rebon.auth.domain.OauthProvider;
-import com.handong.rebon.auth.infrastructure.GoogleRequester;
 import com.handong.rebon.member.presentation.dto.request.MemberCreateRequest;
 
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.Header;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.anyString;
-import static org.mockito.BDDMockito.given;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
 
-    @MockBean
-    GoogleRequester googleRequester;
+    public static final String TEST_EMAIL = "handong@gmail.com";
+
+    private ClientAndServer mockServer;
+
+    @BeforeEach
+    void setUp() {
+        mockServer = ClientAndServer.startClientAndServer(8888);
+
+        new MockServerClient("localhost", 8888)
+                .when(
+                        request()
+                                .withMethod("POST")
+                                .withPath("/token")
+                )
+                .respond(
+                        response()
+                                .withHeader(new Header("Content-Type", "application/json"))
+                                .withBody("{ \"access_token\": \"test_token\"}")
+                );
+        new MockServerClient("localhost", 8888)
+                .when(
+                        request()
+                                .withMethod("GET")
+                                .withPath("/user-info")
+                )
+                .respond(
+                        response()
+                                .withHeader(new Header("Content-Type", "application/json"))
+                                .withBody("{\"email\": " + " \"" + TEST_EMAIL + "\"}")
+                );
+    }
+
+    @AfterEach
+    void shutDown() {
+        mockServer.stop();
+    }
 
     @Test
     @DisplayName("Google 로그인으로 요청이 오면 토큰을 발급한다.")
     void loginByGoogleOauth() {
         //given
         OauthProvider google = OauthProvider.GOOGLE;
-        String testEmail = "test@gmail.com";
         String testNickname = "test";
-        saveMember(new MemberCreateRequest(testEmail, testNickname, "google", true));
+        saveMember(new MemberCreateRequest(TEST_EMAIL, testNickname, "GOOGLE", true));
         String authorizationCode = "test-code";
 
-        given(googleRequester.supports(google))
-                .willReturn(true);
-        given(googleRequester.getUserInfoByCode(anyString()))
-                .willReturn(GoogleUserInfo.from(Map.of("email", testEmail)));
-
         //when
-        ExtractableResponse response = login(google.name(), authorizationCode);
+        ExtractableResponse<Response> response = login(google.name(), authorizationCode);
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.header("Authorization")).isNotNull();
+        assertThat(response.body()).isNotNull();
     }
 
     @Test
-    @DisplayName("미가입 로그인 요청시 404에러와 email을 반환 받는다")
+    @DisplayName("미가입 로그인 요청시 401에러와 email을 반환 받는다")
     void loginByNotRegisteredMemberGetNotFound() {
         //given
         OauthProvider google = OauthProvider.GOOGLE;
-        String testEmail = "test@gmail.com";
         String authorizationCode = "test-code";
 
-        given(googleRequester.supports(google))
-                .willReturn(true);
-        given(googleRequester.getUserInfoByCode(anyString()))
-                .willReturn(GoogleUserInfo.from(Map.of("email", testEmail)));
-
         //when
-        ExtractableResponse response = login(google.name(), authorizationCode);
-        Map responseBody = response.body().as(Map.class);
+        ExtractableResponse<Response> response = login(google.name(), authorizationCode);
+        Map<String, Object> responseBody = response.body().as(new TypeRef<>() {});
 
         //then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
-        assertThat(responseBody.get("email")).isEqualTo(testEmail);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        assertThat(responseBody.get("email")).isEqualTo(TEST_EMAIL);
     }
 
-    private ExtractableResponse login(String oauthProvider, String code) {
+    private ExtractableResponse<Response> login(String oauthProvider, String code) {
         return RestAssured
                 .given(super.spec)
                 .log().all()
@@ -86,7 +112,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     }
 
 
-    private ExtractableResponse<Response> saveMember(MemberCreateRequest memberCreateRequest){
+    private ExtractableResponse<Response> saveMember(MemberCreateRequest memberCreateRequest) {
         return RestAssured.given(super.spec)
                           .log().all()
                           .contentType(APPLICATION_JSON_VALUE)
