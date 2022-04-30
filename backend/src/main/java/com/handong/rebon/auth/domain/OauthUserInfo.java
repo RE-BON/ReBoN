4c5a0a529d9ebd60d4985ecd378dce2bc360e395
PyeongGangKim <21500185@handong.edu>
2022-04-30 18:15:52 +0900

    refactor[#92]: OauthUserInfo를 하나로만 userInfo를 얻는데 사용

--- backend/src/main/java/com/handong/rebon/auth/domain/OauthUserInfo.java
+++ backend/src/main/java/com/handong/rebon/auth/domain/OauthUserInfo.java
@@ -1,5 +1,20 @@
 package com.handong.rebon.auth.domain;
 
-public interface OauthUserInfo {
-    String getEmail();
+import java.util.Collections;
+import java.util.Map;
+
+public class OauthUserInfo {
+    private Map<String, Object> info;
+
+    private OauthUserInfo(Map<String, Object> info) {
+        this.info = Collections.unmodifiableMap(info);
+    }
+
+    public static OauthUserInfo from(Map<String, Object> responseBody) {
+        return new OauthUserInfo(responseBody);
+    }
+
+    public String getEmail() {
+        return (String) info.get("email");
+    }
 }
