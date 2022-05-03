c24acd8a2e6c5b576f77834cde8806faa71a45bc
PyeongGangKim <21500185@handong.edu>
2022-05-03 16:12:15 +0900

    refactor[#119]: info를 field로 갖는 객체로 변환

--- backend/src/main/java/com/handong/rebon/auth/domain/OauthUserInfo.java
+++ backend/src/main/java/com/handong/rebon/auth/domain/OauthUserInfo.java
@@ -1,20 +1,10 @@
 package com.handong.rebon.auth.domain;
 
-import java.util.Collections;
-import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class OauthUserInfo {
-    private Map<String, Object> info;
-
-    private OauthUserInfo(Map<String, Object> info) {
-        this.info = Collections.unmodifiableMap(info);
-    }
-
-    public static OauthUserInfo from(Map<String, Object> responseBody) {
-        return new OauthUserInfo(responseBody);
-    }
-
-    public String getEmail() {
-        return (String) info.get("email");
-    }
+    private String email;
 }
