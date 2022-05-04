75563a51ccda76a4d76f70ab335664d485adfb99
PyeongGangKim <21500185@handong.edu>
2022-05-04 17:55:52 +0900

    test[#142]: nickname 중복 체크 인수테스트

--- backend/src/test/java/com/handong/rebon/acceptance/member/MemberCreateAcceptanceTest.java
+++ backend/src/test/java/com/handong/rebon/acceptance/member/MemberCreateAcceptanceTest.java
@@ -1,8 +1,11 @@
 package com.handong.rebon.acceptance.member;
 
 import com.handong.rebon.acceptance.AcceptanceTest;
+import com.handong.rebon.member.application.MemberService;
+import com.handong.rebon.member.application.dto.request.MemberCreateRequestDto;
 import com.handong.rebon.member.presentation.dto.request.MemberCreateRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +19,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class MemberCreateAcceptanceTest extends AcceptanceTest {
+
+    @Autowired
+    MemberService memberService;
 
     @Test
     @DisplayName("회원 가입을 한다.")
@@ -31,6 +37,33 @@ public class MemberCreateAcceptanceTest extends AcceptanceTest {
         assertThat(response.body()).isNotNull();
     }
 
+    @Test
+    @DisplayName("존재하지 않는 닉네임 중복 체크 요청")
+    void checkDidNotDuplicateNickname() {
+        //given
+        String nickname = "ReBoN";
+        //when
+        ExtractableResponse<Response> response = checkDuplicateNickname(nickname);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("존재하는 닉네임 중복 체크 요청")
+    void checkDuplicatedNickname() {
+        //given
+        String nickname = "test";
+        MemberCreateRequestDto memberCreateRequestDto = new MemberCreateRequestDto("test@gmail.com", "google", nickname, true);
+        memberService.save(memberCreateRequestDto);
+
+        //when
+        ExtractableResponse<Response> response = checkDuplicateNickname(nickname);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
     private ExtractableResponse<Response> saveMember(MemberCreateRequest memberCreateRequest) {
         return RestAssured.given(super.spec)
                           .log().all()
@@ -38,6 +71,18 @@ public class MemberCreateAcceptanceTest extends AcceptanceTest {
                           .body(memberCreateRequest)
                           .when()
                           .post("/api/members")
+                          .then()
+                          .log().all()
+                          .extract();
+    }
+
+    private ExtractableResponse<Response> checkDuplicateNickname(String nickname) {
+        return RestAssured.given(super.spec)
+                          .log().all()
+                          .contentType(APPLICATION_JSON_VALUE)
+                          .body(nickname)
+                          .when()
+                          .post("/api/members/nickname/duplicate")
                           .then()
                           .log().all()
                           .extract();
