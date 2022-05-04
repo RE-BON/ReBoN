8473e954be388c1ec64d196ed1e1fa377f2caebe
PyeongGangKim <21500185@handong.edu>
2022-05-04 17:51:31 +0900

    feat[#142]: nickname duplicate exception 추가

---
+++ backend/src/main/java/com/handong/rebon/exception/member/NicknameDuplicateException.java
@@ -0,0 +1,9 @@
+package com.handong.rebon.exception.member;
+
+import org.springframework.http.HttpStatus;
+
+public class NicknameDuplicateException extends MemberException{
+    public NicknameDuplicateException() {
+        super("nickname이 중복됩니다.", HttpStatus.BAD_REQUEST);
+    }
+}
