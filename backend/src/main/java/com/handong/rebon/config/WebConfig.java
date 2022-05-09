f37833cba799d697ca7005d2e3e84fee6e1f391d
PyeongGangKim <21500185@handong.edu>
2022-05-10 00:30:37 +0900

    feat[#94]: 모든 method에 대해 cors 허용

--- backend/src/main/java/com/handong/rebon/config/WebConfig.java
+++ backend/src/main/java/com/handong/rebon/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("*");
     }
 }
