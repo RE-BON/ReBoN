d950f40ab701a119089cdb0ed6c71d4129023847
PyeongGangKim <21500185@handong.edu>
2022-05-13 17:19:24 +0900

    feat[#134]: 생성자 추가

--- backend/src/main/java/com/handong/rebon/review/application/dto/request/ReviewGetByShopRequestDto.java
+++ backend/src/main/java/com/handong/rebon/review/application/dto/request/ReviewGetByShopRequestDto.java
@@ -2,11 +2,13 @@ package com.handong.rebon.review.application.dto.request;
 
 import org.springframework.data.domain.Pageable;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class ReviewGetByShopRequestDto {
     private Long memberId;
     private Long shopId;
