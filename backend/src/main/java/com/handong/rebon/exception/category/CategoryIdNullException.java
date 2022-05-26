e808daf0349d7b34cfd4cfcb0164dae76ec88a8f
gomster <gomster96@gmail.com>
2022-05-26 17:12:40 +0900

    refactor[#11] : 포맷팅 적용

--- backend/src/main/java/com/handong/rebon/exception/category/CategoryIdNullException.java
+++ backend/src/main/java/com/handong/rebon/exception/category/CategoryIdNullException.java
@@ -3,5 +3,7 @@ package com.handong.rebon.exception.category;
 import org.springframework.http.HttpStatus;
 
 public class CategoryIdNullException extends CategoryException {
-    public CategoryIdNullException() {super("변경하려는 카테고리 id값이 입력되어야 합니다.", HttpStatus.BAD_REQUEST);}
+    public CategoryIdNullException() {
+        super("변경하려는 카테고리 id값이 입력되어야 합니다.", HttpStatus.BAD_REQUEST);
+    }
 }
