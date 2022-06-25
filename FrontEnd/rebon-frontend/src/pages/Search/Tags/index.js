209264d80f3d7a17ea04bbae638af2873dce72dc
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-06-25 09:31:07 +0900

    Fe/style/search get (#184)
    * [#174]: search페이지 tag api GET
    
    * [#174]: search 페이지 api GET
    
    - tag list 를 받았다
    
    * [#174]: search 페이지 api GET
    
    Co-authored-by: 안유진 <Eugenius1st@gmail.com>

--- FrontEnd/rebon-frontend/src/pages/Search/Tags/index.js
+++ FrontEnd/rebon-frontend/src/pages/Search/Tags/index.js
@@ -1,13 +1,27 @@
 import '../../../styles/search-tags.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function Tags() {
+  const [tag, setTags] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://34.238.48.93:8080/api/tags')
+      .then((response) => {
+        setTags(response.data);
+        console.log(tag);
+      })
+      .catch((error) => {
+        console.log('error');
+      });
+  }, []);
+
   return (
     <ul className="tags-wrapper">
       <div>추천 태그로 검색해보세요.</div>
-      {api.map((item) => (
-        <li>{item}</li>
+      {tag.map((item) => (
+        <li>{item.name}</li>
       ))}
     </ul>
   );
 }
-const api = ['영일대', '양덕', '구룡포', '칠포해수욕장', '영일대', '양덕', '구룡포', '칠포해수욕장', '구룡포', '칠포해수욕장'];
