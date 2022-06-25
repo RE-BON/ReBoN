94d357987b75f411b88b0bb8fb8a25d67a1b0c22
안유진 <Eugenius1st@gmail.com>
2022-06-25 09:29:38 +0900

    [#174]: search 페이지 api GET

--- FrontEnd/rebon-frontend/src/pages/Mypage/Edit/index.js
+++ FrontEnd/rebon-frontend/src/pages/Mypage/Edit/index.js
@@ -2,12 +2,31 @@ import '../../../styles/edit.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function Edit() {
   const userName = localStorage.getItem('userName');
-  const [name, setName] = useState(userName);
+  const [name, setName] = useState('');
   const onChangeName = (e) => {
     setName(e.target.value);
+  };
+
+  const checkNick = () => {
+    axios
+      .post('http://34.238.48.93:8080/api/members/nickname/check-duplicate', {
+        nickname: { name },
+      })
+      .then(function (response) {
+        // response
+        console.log(response);
+      })
+      .catch(function (error) {
+        // 오류발생시 실행
+      })
+      .then(function () {
+        // 항상 실행
+        console.log({ name });
+      });
   };
 
   const saveData = (e) => {
@@ -39,7 +58,8 @@ export default function Edit() {
           <div
             className="name-button"
             onClick={() => {
-              setName('');
+              setName('test');
+              checkNick();
             }}
           >
             중복확인
@@ -51,15 +71,15 @@ export default function Edit() {
         <div className="edit-info-marketing">마케팅 수신 동의</div>
         <div className="edit-info-notice ">이메일 수신에 동의하시면 Rebon 주식회사가 제공하는 서비스의 업데이트 및 이벤트 소식을 받을 수 있습니다.</div>
 
-        <div class="select">
+        <div className="select">
           <input type="radio" id="select" name="marcketing" />
-          <label for="select">
+          <label htmlFor="select">
             <FontAwesomeIcon icon={faCheck} />
           </label>
           동의 합니다.
           <span className="empty"></span>
           <input type="radio" id="select2" name="marcketing" />
-          <label for="select2">
+          <label htmlFor="select2">
             <FontAwesomeIcon icon={faCheck} />
           </label>
           동의하지 않습니다.
