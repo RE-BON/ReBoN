caece5649b8fa93cd2b17ddc56f01c1f2911c25d
gomster <gomster96@gmail.com>
2022-06-26 22:02:38 +0900

    test[#136] : 인수 테스트 사용한 요청으로만 처리하도록 구현

--- backend/src/test/java/com/handong/rebon/acceptance/review/ReviewEmpathyAcceptanceTest.java
+++ backend/src/test/java/com/handong/rebon/acceptance/review/ReviewEmpathyAcceptanceTest.java
@@ -1,12 +1,8 @@
 package com.handong.rebon.acceptance.review;
 
-import com.handong.rebon.common.admin.AdminMemberRegister;
-import com.handong.rebon.common.admin.AdminReviewRegister;
-import com.handong.rebon.member.domain.Member;
-import com.handong.rebon.review.domain.Review;
+import com.handong.rebon.review.presentation.dto.request.ReviewRequest;
 import com.handong.rebon.review.presentation.dto.response.ReviewEmpathizeResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import org.junit.jupiter.api.DisplayName;
@@ -18,27 +14,30 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
 import static com.handong.rebon.acceptance.AcceptanceUtils.getRequestSpecification;
+import static com.handong.rebon.acceptance.review.ReviewCreateAcceptanceTest.saveReview;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class ReviewEmpathyAcceptanceTest extends ReviewAcceptanceTest {
 
-    @Autowired
-    private AdminMemberRegister adminMemberRegister;
-
-    @Autowired
-    private AdminReviewRegister adminReviewRegister;
-
     @Test
     @DisplayName("리뷰 공감하기")
     public void empathizeReview() {
         //given
-        Member member = adminMemberRegister.simpleRegister("byungwoong", "byungwoong@gmail.com", "google");
-        Review review = adminReviewRegister.simpleRegister(member, shops.get("팜스발리"), "맛있어요");
-        ExtractableResponse<Response> regitserResponse = 회원가입("test@gmail.com", "test");
+        ExtractableResponse<Response> regitserResponse = 회원가입("gomster96@gmail.com", "gomster");
+        ReviewRequest reviewRequest = new ReviewRequest("맛있어요", "피자는 벌집모양으로!", 5);
         String token = extractedToken(regitserResponse);
+        String bearerToken = "Bearer " + token;
+        ExtractableResponse<Response> reviewCreateResponse = saveReview(
+                reviewRequest, shops.get("팜스발리").getId(), bearerToken);
+
+        String[] locations = reviewCreateResponse.header("location").split("/");
+        long reviewId = Long.parseLong(locations[locations.length - 1]);
+        regitserResponse = 회원가입("test@gmail.com", "test");
+        token = extractedToken(regitserResponse);
+
         //when
-        ExtractableResponse<Response> response = 리뷰_공감하기(token, review);
+        ExtractableResponse<Response> response = 리뷰_공감하기(token, reviewId);
         ReviewEmpathizeResponse result = response.as(new TypeRef<>() {});
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
@@ -50,17 +49,24 @@ public class ReviewEmpathyAcceptanceTest extends ReviewAcceptanceTest {
     @DisplayName("리뷰 공감하기 취소")
     public void unEmpathizeReview() {
         //given
-        Member member = adminMemberRegister.simpleRegister("byungwoong", "byungwoong@gmail.com", "google");
-        Review review = adminReviewRegister.simpleRegister(member, shops.get("팜스발리"), "맛있어요");
-        ExtractableResponse<Response> regitserResponse = 회원가입("test@gmail.com", "test");
+        ExtractableResponse<Response> regitserResponse = 회원가입("gomster96@gmail.com", "gomster");
+        ReviewRequest reviewRequest = new ReviewRequest("맛있어요", "피자는 벌집모양으로!", 5);
         String token = extractedToken(regitserResponse);
+        String bearerToken = "Bearer " + token;
+        ExtractableResponse<Response> reviewCreateResponse = saveReview(
+                reviewRequest, shops.get("팜스발리").getId(), bearerToken);
+
+        String[] locations = reviewCreateResponse.header("location").split("/");
+        long reviewId = Long.parseLong(locations[locations.length - 1]);
+        regitserResponse = 회원가입("test@gmail.com", "test");
+        token = extractedToken(regitserResponse);
 
 
         //when
-        ExtractableResponse<Response> response1 = 리뷰_공감하기(token, review);
+        ExtractableResponse<Response> response1 = 리뷰_공감하기(token, reviewId);
         ReviewEmpathizeResponse result1 = response1.as(new TypeRef<>() {});
 
-        ExtractableResponse<Response> response2 = 리뷰_공감하기_취소(token, review);
+        ExtractableResponse<Response> response2 = 리뷰_공감하기_취소(token, reviewId);
         ReviewEmpathizeResponse result2 = response2.as(new TypeRef<>() {});
         //then
         assertThat(result1.getEmpathizeCount()).isEqualTo(1);
@@ -69,27 +75,28 @@ public class ReviewEmpathyAcceptanceTest extends ReviewAcceptanceTest {
         assertThat(result2.getEmpathizeCount()).isEqualTo(0);
     }
 
-    private ExtractableResponse<Response> 리뷰_공감하기(String token, Review review) {
+    private ExtractableResponse<Response> 리뷰_공감하기(String token, Long reviewId) {
         return RestAssured.given(getRequestSpecification())
                           .log().all()
                           .header("Authorization", "Bearer " + token)
                           .contentType(APPLICATION_JSON_VALUE)
                           .when()
-                          .post("/api/" + review.getId() + "/empathize")
+                          .post("/api/reviews/" + reviewId + "/empathize")
                           .then()
                           .log().all()
                           .extract();
     }
 
-    private ExtractableResponse<Response> 리뷰_공감하기_취소(String token, Review review) {
+    private ExtractableResponse<Response> 리뷰_공감하기_취소(String token, Long reviewId) {
         return RestAssured.given(getRequestSpecification())
                           .log().all()
                           .header("Authorization", "Bearer " + token)
                           .contentType(APPLICATION_JSON_VALUE)
                           .when()
-                          .post("/api/" + review.getId() + "/unempathize")
+                          .post("/api/reviews/" + reviewId + "/unempathize")
                           .then()
                           .log().all()
                           .extract();
     }
+
 }
