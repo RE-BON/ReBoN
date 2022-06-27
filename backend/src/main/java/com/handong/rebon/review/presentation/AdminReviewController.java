45cab0c82b6ca85d7d2e96037a93944c0556009c
PyeongGangKim <21500185@handong.edu>
2022-06-27 21:31:17 +0900

    feat[#148]: get review admin list 기능 추가

--- backend/src/main/java/com/handong/rebon/review/presentation/AdminReviewController.java
+++ backend/src/main/java/com/handong/rebon/review/presentation/AdminReviewController.java
@@ -1,27 +1,38 @@
 package com.handong.rebon.review.presentation;
 
-import com.handong.rebon.review.application.ReviewService;
+import java.util.List;
 
+import com.handong.rebon.review.application.ReviewService;
+import com.handong.rebon.review.application.dto.request.AdminReviewGetRequestDto;
+import com.handong.rebon.review.application.dto.response.AdminReviewResponseDto;
+import com.handong.rebon.review.presentation.dto.request.ReviewSearchRequest;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RequestMapping("/admin/reviews")
+@RequestMapping("/admin")
 @Controller
 public class AdminReviewController {
 
     private final ReviewService reviewService;
 
-    @GetMapping("/")
-    public String getReviews() {
-        return "";
-    }
-
-    @GetMapping("/{reviewId}")
-    public String getReview() {
-        return "";
+    @GetMapping("/reviews")
+    public String getReviews(
+            @ModelAttribute("reviewSearchRequest") ReviewSearchRequest reviewSearchRequest,
+            @PageableDefault Pageable pageable, Model model
+    ) {
+        AdminReviewGetRequestDto adminReviewGetRequestDto = new AdminReviewGetRequestDto(reviewSearchRequest.getKeyword(), pageable);
+        List<AdminReviewResponseDto> reviews = reviewService.search(adminReviewGetRequestDto);
+        model.addAttribute("reviews", reviews);
+        return "review/list";
     }
 }
