dc87cfb371cdea5a7c59132a8f08e53660a1ef04
leehyelim0691 <48082589+leehyelim0691@users.noreply.github.com>
2022-06-28 00:26:07 +0900

    Detail 페이지 지도 구현 (#201)

---
+++ FrontEnd/rebon-frontend/src/pages/Detail/Map/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+const { kakao } = window;
+
+export default function Map({ searchPlace }) {
+  useEffect(() => {
+    const container = document.getElementById('myMap');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(searchPlace, placesSearchCB);
+
+    function placesSearchCB(data, status, pagination) {
+      if (status === kakao.maps.services.Status.OK) {
+        let bounds = new kakao.maps.LatLngBounds();
+
+        for (let i = 0; i < data.length; i++) {
+          displayMarker(data[i]);
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        map.setBounds(bounds);
+      }
+    }
+    function displayMarker(place) {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x),
+      });
+    }
+  }, [searchPlace]);
+
+  return (
+    <div
+      id="myMap"
+      style={{
+        width: '500px',
+        height: '500px',
+      }}
+    ></div>
+  );
+}
