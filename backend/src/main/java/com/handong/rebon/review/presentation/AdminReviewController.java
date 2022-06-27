562c99ff3c532ec9692877787779d5b04d939d01
PyeongGangKim <21500185@handong.edu>
2022-06-27 21:32:38 +0900

    refactor[#148]: reformatting

--- backend/src/main/java/com/handong/rebon/review/presentation/AdminReviewController.java
+++ backend/src/main/java/com/handong/rebon/review/presentation/AdminReviewController.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
