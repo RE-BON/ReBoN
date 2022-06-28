520d1fb4ff232387e7726c55dcd0398e0de8c209
hagus99 <hagus99@kakao.com>
2022-06-29 00:35:03 +0900

    style:main css 수정

--- FrontEnd/rebon-frontend/src/pages/Main/Divider/index.js
+++ FrontEnd/rebon-frontend/src/pages/Main/Divider/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../../../styles/main.css';
+import Toggle from 'react-toggle';
 
 export default function Divider() {
+  const [toggleOn, setToggleOn] = useState(false);
+
+  const toggleChange = (event) => {
+    if (toggleOn) setToggleOn(false);
+    else setToggleOn(true);
+    // console.log(event);
+  };
   return (
     <div className="divider-main">
       <div className="searchTitle">'칠포해수욕장'식당</div>
-      <select className="filter" name="filter">
-        <option value="star" selected>
-          별점순
-        </option>
-        <option value="review">리뷰많은 순</option>
-        <option value="recent">최신순</option>
-      </select>
+      <div>
+        <Toggle className="review-toggle" id="cheese-status" defaultChecked={toggleOn} onChange={toggleChange} />
+        <div>현재 영업중</div>
+        <select className="filter" name="filter">
+          <option value="star" selected>
+            별점순
+          </option>
+          <option value="review">리뷰많은 순</option>
+          <option value="recent">최신순</option>
+        </select>
+      </div>
     </div>
   );
 }
