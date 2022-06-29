3767ed4a8c8bff998ebf4fb8824332b16a0a5865
KJunseo <air.junseo@gmail.com>
2022-06-30 00:15:24 +0900

    test

--- backend/src/test/java/com/handong/rebon/config/TestElasticSearchConfig.java
+++ backend/src/test/java/com/handong/rebon/config/TestElasticSearchConfig.java
@@ -1,27 +1,27 @@
-package com.handong.rebon.config;
-
-import com.handong.rebon.acceptance.tag.TagElasticSearchAcceptanceTest;
-import com.handong.rebon.tag.domain.repository.TagSearchRepository;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import org.elasticsearch.client.RestHighLevelClient;
-
-@EnableElasticsearchRepositories(basePackageClasses = {TagSearchRepository.class})
-@TestConfiguration
-public class TestElasticSearchConfig extends AbstractElasticsearchConfiguration {
-
-    @Override
-    public RestHighLevelClient elasticsearchClient() {
-        ClientConfiguration clientConfiguration =
-                ClientConfiguration.builder()
-                                   .connectedTo(TagElasticSearchAcceptanceTest.CONTAINER.getHost() +
-                                           ":" + TagElasticSearchAcceptanceTest.CONTAINER.getMappedPort(9200))
-                                   .build();
-        return RestClients.create(clientConfiguration).rest();
-    }
-}
+//package com.handong.rebon.config;
+//
+//import com.handong.rebon.acceptance.tag.TagElasticSearchAcceptanceTest;
+//import com.handong.rebon.tag.domain.repository.TagSearchRepository;
+//
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.data.elasticsearch.client.ClientConfiguration;
+//import org.springframework.data.elasticsearch.client.RestClients;
+//import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//
+//import org.elasticsearch.client.RestHighLevelClient;
+//
+//@EnableElasticsearchRepositories(basePackageClasses = {TagSearchRepository.class})
+//@TestConfiguration
+//public class TestElasticSearchConfig extends AbstractElasticsearchConfiguration {
+//
+//    @Override
+//    public RestHighLevelClient elasticsearchClient() {
+//        ClientConfiguration clientConfiguration =
+//                ClientConfiguration.builder()
+//                                   .connectedTo(TagElasticSearchAcceptanceTest.CONTAINER.getHost() +
+//                                           ":" + TagElasticSearchAcceptanceTest.CONTAINER.getMappedPort(9200))
+//                                   .build();
+//        return RestClients.create(clientConfiguration).rest();
+//    }
+//}
