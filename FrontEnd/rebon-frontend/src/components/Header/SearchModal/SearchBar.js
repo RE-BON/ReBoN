057efbf719d7b04ede3fa3866a03846c922e5c8b
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-07-05 22:49:37 +0900

    [#230]: terms 페이지 (#248)
    * [#220]: Search 페이지 Api 연결
    
    * [#230]: termsPage
    
    Co-authored-by: 안유진 <Eugenius1st@gmail.com>

--- FrontEnd/rebon-frontend/src/components/Header/SearchModal/SearchBar.js
+++ FrontEnd/rebon-frontend/src/components/Header/SearchModal/SearchBar.js
@@ -1,9 +1,15 @@
 import '../../../styles/header-search-bar.css';
+import React, { useState } from 'react';
 
 export default function Searchbar() {
+  const [word, setWord] = useState('');
+  const onChangeWord = (e) => {
+    setWord(e.target.value);
+    console.log(word);
+  };
   return (
     <div className="search-bar-wrapper">
-      <input placeholder="가고 싶은 지역을 입력해주세요" />
+      <input placeholder="가고 싶은 지역을 입력해주세요." value={word} onChange={onChangeWord} />
       <img src="../../../../image/search-icon.png" alt="header-search-icon" />
     </div>
   );
