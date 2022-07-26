e310915b95f7deeb1c47b2170094a01378471166
hagus99 <hagus99@kakao.com>
2022-07-26 17:18:07 +0900

    북마크 css 수정

--- FrontEnd/rebon-frontend/src/pages/Mypage/Bookmark/BookmarkCard/index.js
+++ FrontEnd/rebon-frontend/src/pages/Mypage/Bookmark/BookmarkCard/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import '../../../../styles/bookmark-card.css';
 import { FiHeart } from 'react-icons/fi';
+import { FaHeart } from 'react-icons/fa';
 
 export default function BookmarkCard() {
   return (
-    <div className="mainCard">
-      <div className="mainCard-img">
-        <FiHeart md={8} size="22" />
+    <>
+      <div className="bookmarkCard">
+        <div className="bookmarkCard-img">
+          <FiHeart md={8} size="22" />
+        </div>
+        <div className="titleRow">
+          <div className="placeName">Place one</div>
+          <div className="starNum">4.9</div>
+        </div>
+        <ul className="tag-wrapper">
+          <li className="tag">칠포해수욕장</li>
+          <li className="tag">칠포읍</li>
+          <li className="tag">한식</li>
+        </ul>
       </div>
-      <div className="titleRow">
-        <div className="placeName">Place one</div>
-        <div className="starNum">4.9</div>
+      {/* /////////////////////// 모바일 /////////////////////// */}
+      <div className="bookmarkCard-mobile">
+        {/* <img class="main-img" src={item.image} /> */}
+
+        {/* <div className="likeBtn-main">
+          <FaHeart className="heart-icon" md={8} size="22" onClick={likeClick} />
       </div>
-      <ul className="tag-wrapper">
-        <li className="tag">칠포해수욕장</li>
-        <li className="tag">칠포읍</li>
-        <li className="tag">한식</li>
-      </ul>
-    </div>
+      <div className="mainCard-bottom">
+        <div className="titleRow">
+            <div className="placeName-main">식당이름</div>
+          <div className="starNum">5</div>
+        </div>
+        <div className="">
+            <span className="tag">한동대</span>
+        </div>
+      </div> */}
+      </div>
+    </>
   );
 }
