792a147fd24680c7e2cfc784b42e8ed797a8d407
KJunseo <air.junseo@gmail.com>
2022-08-16 15:28:07 +0900

    test: api 문서화

--- backend/src/main/java/com/handong/rebon/shop/presentation/dto/request/NaverShopRequest.java
+++ backend/src/main/java/com/handong/rebon/shop/presentation/dto/request/NaverShopRequest.java
@@ -1,10 +1,12 @@
 package com.handong.rebon.shop.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class NaverShopRequest {
     private String keyword;
     private int displayCount;
